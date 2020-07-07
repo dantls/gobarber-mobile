a4ecb5e4c6bdb46dfc02ce0317c17f0e7129891c
dantls <dan_tls@hotmail.com>
2020-07-06 20:37:14 -0400

    API adjustments

---
+++ src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const AppointmentCreated: React.FC = () => {
+  return (
+    // <Container>
+    <View />
+    // </Container>
+  );
+};
+export default AppointmentCreated;
